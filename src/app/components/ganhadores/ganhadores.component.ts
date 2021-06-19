6d538f649a231c518b1920cff60ede45eaf644c5
Thais Roos Gularte <thaisrrg@hotmail.com>
2021-06-19 17:17:19 -0300

    final

--- src/app/components/ganhadores/ganhadores.component.ts
+++ src/app/components/ganhadores/ganhadores.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OlimpiadasMedalhaApiService } from 'src/app/services/olimpiadas-medalha-api.service';
+import { OlimpiadasMedalhaModel } from 'src/app/services/olimpiadas-medalha-model';
 
 @Component({
   selector: 'app-ganhadores',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ganhadores.component.css']
 })
 export class GanhadoresComponent implements OnInit {
+  displayedColumns: string[] = ['posicao', 'pais', 'ouro','prata','bronze'];
+  dataSource: OlimpiadasMedalhaModel[] = [];
 
-  constructor() { }
+  constructor(public medalhasApi: OlimpiadasMedalhaApiService) { }
 
   ngOnInit(): void {
+    this.medalhasApi.get().subscribe({
+      next: (retornoDaApi) => {
+        this.dataSource = retornoDaApi;
+      }
+    });
   }
+
 
 }
